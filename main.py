3b495ad70c86324b3ea32eb11338c9cb6238382e
Krishanus-2001 <krishanu21saini@gmail.com>
2021-05-30 00:19:38 +0530

    Add api

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def cronjob():
     print("Cron job is running")
     print("Tick! The time is: %s" % datetime.now())
     response = requests.get("https://mernstackestore.herokuapp.com/items")
-    text = json.dumps(response, sort_keys=True, indent=4)
+    text = json.dumps(response.json(), sort_keys=True, indent=4)
     print(text)
