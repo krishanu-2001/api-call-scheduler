cc2b805315ef2fc32415262642da84b1ae042d00
Krishanus-2001 <krishanu21saini@gmail.com>
2021-06-03 10:34:15 +0530

    Add cronjob fil;e

--- cronjob.py
+++ cronjob.py
@@ -1,10 +1,7 @@
-# Package Scheduler.
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-# Main cronjob function.
 from main import cronjob
 
-# Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
 scheduler.add_job(cronjob, "interval", seconds=30)
 
