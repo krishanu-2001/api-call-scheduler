b91b7fd8a10ec178b362c69bf0450cef0b3aad5a
Krishanus-2001 <krishanu21saini@gmail.com>
2021-06-03 10:33:55 +0530

    Add logic

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import requests
 import json
 
 def cronjob():
-    """
-    Main cron job.
-    The main cronjob to be run continuously.
-    """
     print("Cron job is running")
     print("Tick! The time is: %s" % datetime.now())
     response = requests.get("https://mernstackestore.herokuapp.com/items")
